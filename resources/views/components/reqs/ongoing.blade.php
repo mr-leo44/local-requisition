06272220db54f23414acabb7175ceaad51817739
mr-leo44 <lionnelkaniki44@gmail.com>
2024-09-19 13:55:15 +0100

    drop to deliver on gridCard

--- resources/views/components/reqs/ongoing.blade.php
+++ resources/views/components/reqs/ongoing.blade.php
@@ -73,16 +73,7 @@
                         <p class="font-medium text-md text-gray-700 dark:text-white">{{ $req->service }}</p>
                         <p class="font-medium text-md text-gray-700 dark:text-white">{{ $req->user->compte->city }}</p>
                     </div>
-                    <div class="flex justify-between items-center p-4">
-                        <div>
-                            <p class="text-gray-700 dark:text-gray-400 text-sm">{{ $req->to_deliver }}
-                                @if ($req->to_deliver > 1)
-                                    {{ __('Pièces') }}
-                                @else
-                                    {{ __('Pièce') }}
-                                @endif à livrer
-                            </p>
-                        </div>
+                    <div class="flex justify-end items-center ml-auto p-4">
                         <div class="flex justify-end items-center gap-2">
                             <button data-modal-target="show-modal" data-modal-toggle="show-modal" type="button"
                                 class="bg-orange-500 px-3 py-2 rounded" onclick="showModal({{ $req }})">
